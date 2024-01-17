12f0bee6229d6eace1faaa32fd3433001beb509f
Kat <katrinaburwash_17@hotmail.com>
2024-01-17 10:15:20 +0100

    made button logic. used state

--- src/CalculatorContainer.jsx
+++ src/CalculatorContainer.jsx
@@ -2,11 +2,27 @@ import Button from "./Buttons";
 import { useState } from "react";
 
 function CalculatorContainer(){
-    const buttons = [1,2,3,4,5,6,7,8,9,0,"%", "x", "/","+","="]
+    const buttons = [1,2,3,4,5,6,7,8,9,0,"-", "x", "/","+","="]
     const [inputValue, setInputValue] = useState('')
+    const [calculation, setCalculation] = useState('')
 
     const handleClick = (button) => {
-        setInputValue(button)
+        if (button === '=') {
+            try {
+                const result = eval(calculation);
+                setInputValue(result.toString());
+                setCalculation('');
+            } catch (error) {
+                setInputValue('Error');
+                setCalculation('');
+            }
+        } else if (button === 'x') {
+            setCalculation((prevCalc) => prevCalc + '*');
+            setInputValue((prevInput) => prevInput + button);
+        } else {
+            setCalculation((prevCalc) => prevCalc + button);
+            setInputValue((prevInput) => prevInput + button);
+        }
     }
 
     const btnDigit = buttons.map((num,i) => {
@@ -19,15 +35,15 @@ function CalculatorContainer(){
 
     return (
         <div className="calc-container">
-            {/* Calc-info-container not flexing correctly.... It is supposed to have space between each child. */}
+            
             <div className="calc-info-container">
                 <div className="input-field">{inputValue}</div>
-                <h1>CE</h1>
-                <h1>Ans.</h1>
+                <h1 className="clear">CE</h1>
+                <h1 className="answer-field">Ans.</h1>
             </div>
 
             <div className="btns-container">
-            {btnDigit}
+                {btnDigit}
             </div>
             
         </div>
